c807973a6ea0bd6934b5673d78cfc5e75087acf1
evangel <evangeldaping@gmail.com>
2022-03-23 19:58:32 +0800

    Optimising Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.yellow,
-      body: Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(fontSize: 30, color: Colors.white),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        body: Center(
+          child: Text(
+            "Hello World!",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
