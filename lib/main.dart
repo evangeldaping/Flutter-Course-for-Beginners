ff983cba5019458bcb4659c969b09e53be017f09
evangel <evangeldaping@gmail.com>
2022-03-23 20:12:02 +0800

    UI Basics - Container widget with multiple properties

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green[800],
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          title: Text("App Name"),
-        ),
-      ),
+          backgroundColor: Colors.green[800],
+          appBar: AppBar(
+            backgroundColor: Colors.indigo,
+            title: Text("App Name"),
+          ),
+          body: Container(
+              padding: EdgeInsets.all(8.0),
+              margin: EdgeInsets.all(12.0),
+              height: 100,
+              width: 200,
+              color: Colors.red,
+              child: Text("Hello"))),
     );
   }
 }
