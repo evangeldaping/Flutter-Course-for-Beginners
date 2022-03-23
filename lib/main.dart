2bbfa22f130694645b32cb307bb1b34ac45a5794
evangel <evangeldaping@gmail.com>
2022-03-23 21:45:39 +0800

    UI Basics - Social Card Exercise

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,91 @@ class MyApp extends StatelessWidget {
           title: Text("App Name"),
         ),
         body: Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: Center(
+          child: Center(
               child: Container(
-                  color: Colors.green,
-                  height: 300,
-                  width: double.infinity,
+            height: 280,
+            color: Colors.grey,
+            margin: EdgeInsets.all(8),
+            child: Column(children: [
+              Container(
+                  padding: EdgeInsets.all(4.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      CircleAvatar(
-                        backgroundImage: AssetImage("images/evangel.jpg"),
-                      ),
                       Container(
-                        width: 20,
-                      ), // use for spacing
-                      Text("AppMaking"),
-                      Text("Learn flutter"),
-                      SizedBox(
-                        width: 40,
-                      ), // use for spacing
-                      ElevatedButton(onPressed: () {}, child: Text("Contact"))
+                          child: CircleAvatar(
+                        backgroundImage: AssetImage("images/evangel.jpg"),
+                      )),
+                      SizedBox(width: 20),
+                      Expanded(
+                        // Expanded wil occupy the remaining space
+                        child: Container(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("AppMaking"),
+                                Text("Learn Flutter"),
+                              ]),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Container(
+                        child: Icon(Icons.more_vert),
+                      ),
                     ],
                   )),
-            )),
+              Container(
+                  height: 200,
+                  width: double.infinity,
+                  child: Image.network(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
+                    fit: BoxFit.cover,
+                  )),
+              Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                          child: Row(
+                        children: [
+                          Container(
+                              child: Row(
+                            children: [
+                              Icon(Icons.thumb_up),
+                              Text("Like"),
+                            ],
+                          ))
+                        ],
+                      )),
+                      Container(
+                          child: Row(
+                        children: [
+                          Container(
+                              child: Row(
+                            children: [
+                              Icon(Icons.message),
+                              Text("Comment"),
+                            ],
+                          ))
+                        ],
+                      )),
+                      Container(
+                          child: Row(
+                        children: [
+                          Container(
+                              child: Row(
+                            children: [
+                              Icon(Icons.share),
+                              Text("Share"),
+                            ],
+                          ))
+                        ],
+                      )),
+                    ]),
+              ),
+            ]),
+          )),
+        ),
       ),
     );
   }
